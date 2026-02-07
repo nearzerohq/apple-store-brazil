54d5103f2cd6a2783451350e3a17b14942145235
nearzerohq <nearzero.dev@gmail.com>
2026-02-08 04:55:26 +0530

    Commit created by Cluster

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import Hero from '../components/Hero';
-import ProductGrid from '../components/ProductGrid';
-import FeaturedProducts from '../components/FeaturedProducts';
+import React from "react";
+import Hero from "../components/Hero";
+import ProductGrid from "../components/ProductGrid";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 const Home = () => {
   return (
     <main>
-      <Hero />
       <ProductGrid />
       <FeaturedProducts />
     </main>
